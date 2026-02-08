40de9a703e872457a54dc07fc9a6839f6287a6bf
ARTEMIJKRUTIN <124494886+ARTEMIJKRUTIN@users.noreply.github.com>
2026-02-08 09:41:13 +0100

    Add files via upload

--- ARTEMIJKRUTIN/online_shop/app/routes.py
+++ ARTEMIJKRUTIN/online_shop/app/routes.py
@@ -4,7 +4,8 @@ bp=Blueprint('routes', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    products= Product.query.all()
+    return render_template('index.html', products=products)
 
 @bp.route('/products')
 def product():
@@ -22,3 +23,38 @@ def add_product():
         flash('Product added!')
         return redirect(url_for('routes.product'))
     return render_template('product_form.html', action='Add', product=None)
+
+
+@bp.route('/delete/<int:product_id>', methods=['POST'])
+def delete_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    db.session.delete(product)
+    db.session.commit()
+    flash('Product deleted!')
+    return redirect(url_for('routes.product'))
+
+@bp.route('/update/<int:product_id>', methods=['GET', 'POST'])
+def update_product(product_id):
+    product = Product.query.get_or_404(product_id)
+    if request.method == 'POST':
+        product.name = request.form['name']
+        product.price = float(request.form['price'])
+        product.desription = request.form['desription']
+        product.stock = request.form['stock']
+        product.is_active = request.form['is_active']
+        product.category = request.form['category']
+        product.rating = float(request.form['rating'])
+        product.sale = request.form['sale']
+        db.session.commit()
+        flash('Product updated!')
+        return redirect(url_for('routes.product'))
+    
+    
+    
+    print (f'''name={product.name}, price={product.price}, description={product.description}, stock={product.stock}, is_active={product.is_active}, category={product.category}, rating={product.rating}, sale={product.sale}''')
+
+    
+    
+    
+    
+    return render_template('product_form.html', action='Update', product=product)

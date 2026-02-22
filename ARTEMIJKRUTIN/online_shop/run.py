07d20e49a44c9ccc5295597ceb15b92436534608
ARTEMIJKRUTIN <124494886+ARTEMIJKRUTIN@users.noreply.github.com>
2026-02-22 10:13:24 +0100

    Add files via upload

--- ARTEMIJKRUTIN/online_shop/run.py
+++ ARTEMIJKRUTIN/online_shop/run.py
@@ -7,4 +7,6 @@ if __name__ == '__main__':
     # Create DB tables if not exist
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
